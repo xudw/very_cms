c62644851a55c7909ab193d65240de72dc3943b0
徐东伟 <xudongwei@starschina.com>
2015-07-27 17:21:22 +0800

    add

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,12 +5,35 @@ class IndexController extends Controller {
 
     public function index(){
 
-	    $htmltable = D('very_html');//应用
+	    $htmltable = D('very_html');
 	    
 	    $sql = "select page_name from very_html where nick_name='首页' order by mtime desc limit 1";
 	    $newpage = $htmltable->query($sql);
 
-    	// $newpage = 's'.date('Ymd');
+        $this->display($newpage[0]['page_name']);
+    }
+
+    public function product(){
+
+        $id = htmlspecialchars(addslashes($_GET['pid']));
+
+        $htmltable = D('very_html');
+
+        $page = $id.'_p';
+        $sql = "select page_name from very_html where page_name='$page' and nick_name='产品' order by mtime desc limit 1";
+        $newpage = $htmltable->query($sql);
+        $this->display($newpage[0]['page_name']);
+    }
+
+    public function news(){
+
+        $id = htmlspecialchars(addslashes($_GET['pid']));
+
+        $htmltable = D('very_html');
+
+        $page = $id.'_n';
+        $sql = "select page_name from very_html where page_name='$page' and nick_name='新闻' order by mtime desc limit 1";
+        $newpage = $htmltable->query($sql);
         $this->display($newpage[0]['page_name']);
     }
 }
